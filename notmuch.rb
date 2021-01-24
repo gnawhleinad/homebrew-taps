4cd3a61409e1ce326312354a416d958dff99f093
Daniel Hwang <danielleehwang@gmail.com>
2021-01-23 17:46:04 -0800

    make ruby-bindings

--- notmuch.rb
+++ notmuch.rb
@@ -12,20 +12,13 @@ class Notmuch < Formula
     regex(/href=.*?notmuch[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
-  bottle do
-    cellar :any
-    sha256 "50463927fdfdbd0c2ce451b058d62411c1103224b782594a7c0e31e5cfb6b62d" => :big_sur
-    sha256 "adf2064697804a4c19a1840b14e2ad346062ad27511eccee87cf12d9d772f83d" => :arm64_big_sur
-    sha256 "ce1daba205ab1785008998e93dcb203c06afba8967dece326fec8a868b08de97" => :catalina
-    sha256 "b3dff4eefeaa3438b0b3c87c36b6fb2b3c41da2cdc59e1870a9f8eb1348d4392" => :mojave
-  end
-
   depends_on "doxygen" => :build
   depends_on "libgpg-error" => :build
   depends_on "pkg-config" => :build
   depends_on "sphinx-doc" => :build
   depends_on "glib"
   depends_on "gmime"
+  depends_on "ruby@3.0"
   depends_on "python@3.9"
   depends_on "talloc"
   depends_on "xapian"
@@ -39,7 +32,6 @@ class Notmuch < Formula
       --emacsetcdir=#{elisp}
       --bashcompletiondir=#{bash_completion}
       --zshcompletiondir=#{zsh_completion}
-      --without-ruby
     ]
 
     ENV.append_path "PYTHONPATH", Formula["sphinx-doc"].opt_libexec/"lib/python3.9/site-packages"
@@ -56,6 +48,9 @@ class Notmuch < Formula
     cd "bindings/python" do
       system Formula["python@3.9"].opt_bin/"python3", *Language::Python.setup_install_args(prefix)
     end
+
+    ENV.prepend_path "PATH", Formula["ruby@3.0"].opt_bin
+    system "make", "ruby-bindings"
   end
 
   test do
